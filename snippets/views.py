85f743626b3f26fd6410b5d3f0b54443a5620bbf
sajbon <siboniecjr@gmail.com>
2023-04-01 21:47:35 +0200

    Sending files

--- snippets/views.py
+++ snippets/views.py
@@ -3,5 +3,3 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-git config --global user.email "you@example.com"
-  git config --global user.name "Your Name"
